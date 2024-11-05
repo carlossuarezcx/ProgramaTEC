bc6d701f6de6c746ff7fc17e72212d916ec7c2c2
Carlos Suárez <57473696+carlossuarezcx@users.noreply.github.com>
2024-11-05 15:04:58 -0600

    motores

--- program/program.ino
+++ program/program.ino
@@ -1,6 +1,14 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
 
+#define IN1 D1
+#define IN2 D2
+#define IN3 D3
+#define IN4 D4
+#define ENA D5
+#define ENB D6
+const int velocidad = 255;
+
 const char* ssid = "HOME";
 const char* password = "3029251610";
 const int tiempoEspera = 3000;
@@ -12,16 +20,16 @@ void procesarSecuencia(String secuencia) {
         char movimiento = secuencia.charAt(i);
         switch (movimiento) {
             case 'W':
-                moverAdelante();
+                adelante();
                 break;
             case 'S':
-                moverAtras();
+                atras();
                 break;
             case 'A':
-                moverIzquierda();
+                izquierda();
                 break;
             case 'D':
-                moverDerecha();
+                derecha();
                 break;
             default:
                 Serial.println("Comando desconocido: " + String(movimiento));
@@ -30,26 +38,52 @@ void procesarSecuencia(String secuencia) {
         delay(tiempoEspera);
     }
 }
-void moverAdelante() {
-    Serial.println("Moviendo hacia adelante");
+void adelante() {
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
 }
 
-void moverAtras() {
-    Serial.println("Moviendo hacia atrás");
+void atras() {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
 }
 
-void moverIzquierda() {
-    Serial.println("Moviendo a la izquierda");
+void izquierda() {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
 }
 
-void moverDerecha() {
-    Serial.println("Moviendo a la derecha");
+void derecha() {
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
 }
 
+void detenerMotores() {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
+}
 void setup() {
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+  pinMode(ENA, OUTPUT);
+  pinMode(ENB, OUTPUT);
+
   Serial.begin(115200);
   WiFi.begin(ssid, password);
   Serial.println("");
+
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
@@ -59,12 +93,16 @@ void setup() {
   Serial.print("IP: ");
   Serial.println(WiFi.localIP());
 
-  server.on("/play", HTTP_POST, play);
-  server.on("/stop", HTTP_POST, stop);
+  server.on("/play", HTTP_POST, iniciar);
+  server.on("/stop", HTTP_POST, detener);
   server.begin();
+
+  
+  analogWrite(ENA, velocidad);
+  analogWrite(ENB, velocidad);
 }
 
-void play() {
+void iniciar() {
   server.sendHeader("Access-Control-Allow-Origin", "*");
   if (server.hasArg("plain")) {
     String secuencia = server.arg("plain");
@@ -74,15 +112,11 @@ void play() {
     server.send(400, "text/plain", "No se recibió secuencia");
   }
 }
-void stop() {
+void detener() {
   server.sendHeader("Access-Control-Allow-Origin", "*");
-  if (server.hasArg("plain")) {
-    String secuencia = server.arg("plain");
-    server.send(200, "text/plain", "Deteniendo");
-    Serial.println("Detenido");
-  } else {
-    server.send(400, "text/plain", "No se recibió secuencia");
-  }
+  detenerMotores();
+  server.send(200, "text/plain", "Motores detenidos");
+  Serial.println("Detenido");
 }
 void loop() {
   server.handleClient();
