63c0cc22d58bc504d58fb4157d0f3195cdf73e16
Carlos Suárez <57473696+carlossuarezcx@users.noreply.github.com>
2024-11-06 20:51:17 -0600

    actualización

--- program/program.ino
+++ program/program.ino
@@ -7,11 +7,12 @@
 #define IN4 D4
 #define ENA D5
 #define ENB D6
-const int velocidad = 255;
 
-const char* ssid = "HOME";
-const char* password = "3029251610";
-const int tiempoEspera = 3000;
+int velocidad = 100;
+int tiempo = 1000;
+
+const char* ssid = "PC";
+const char* password = "68+1O2h8";
 
 ESP8266WebServer server(80);
 
@@ -35,38 +36,54 @@ void procesarSecuencia(String secuencia) {
                 Serial.println("Comando desconocido: " + String(movimiento));
                 break;
         }
-        delay(tiempoEspera);
     }
 }
 void adelante() {
+  Serial.println("  Adelante");
   digitalWrite(IN1, HIGH);
   digitalWrite(IN2, LOW);
   digitalWrite(IN3, HIGH);
   digitalWrite(IN4, LOW);
+  delay(tiempo);
+  Serial.println("  Detener");
+  detenerMotores();
 }
 
 void atras() {
+  Serial.println("  Atrás");
   digitalWrite(IN1, LOW);
   digitalWrite(IN2, HIGH);
   digitalWrite(IN3, LOW);
   digitalWrite(IN4, HIGH);
+  delay(tiempo);
+  Serial.println("  Detener");
+  detenerMotores();
 }
 
 void izquierda() {
+  Serial.println("  Izquierda");
   digitalWrite(IN1, LOW);
   digitalWrite(IN2, HIGH);
   digitalWrite(IN3, HIGH);
   digitalWrite(IN4, LOW);
+  delay(tiempo);
+  Serial.println("  Detener");
+  detenerMotores();
 }
 
 void derecha() {
+  Serial.println("  Derecha");
   digitalWrite(IN1, HIGH);
   digitalWrite(IN2, LOW);
   digitalWrite(IN3, LOW);
   digitalWrite(IN4, HIGH);
+  delay(tiempo);
+  Serial.println("  Detener");
+  detenerMotores();
 }
 
 void detenerMotores() {
+  Serial.println("  DETENIDO ");
   digitalWrite(IN1, LOW);
   digitalWrite(IN2, LOW);
   digitalWrite(IN3, LOW);
@@ -88,16 +105,18 @@ void setup() {
     delay(500);
     Serial.print(".");
   }
-  Serial.println("Conectado a WiFi!");
+  Serial.println("Conectado a WiFi");
 
   Serial.print("IP: ");
   Serial.println(WiFi.localIP());
 
   server.on("/play", HTTP_POST, iniciar);
   server.on("/stop", HTTP_POST, detener);
+  server.on("/velocidad", HTTP_POST, setVelocidad);
+  server.on("/tiempo", HTTP_POST, setTiempo);
+
   server.begin();
 
-  
   analogWrite(ENA, velocidad);
   analogWrite(ENB, velocidad);
 }
@@ -107,6 +126,7 @@ void iniciar() {
   if (server.hasArg("plain")) {
     String secuencia = server.arg("plain");
     server.send(200, "text/plain", "Secuencia procesada");
+    Serial.println("Nueva secuencia:" + secuencia);
     procesarSecuencia(secuencia);
   } else {
     server.send(400, "text/plain", "No se recibió secuencia");
@@ -117,6 +137,34 @@ void detener() {
   detenerMotores();
   server.send(200, "text/plain", "Motores detenidos");
   Serial.println("Detenido");
+}
+void setVelocidad(){
+  server.sendHeader("Access-Control-Allow-Origin", "*");
+  if (server.hasArg("plain")) {
+    int nuevaVelocidad = server.arg("plain").toInt();
+    if(nuevaVelocidad >=0 && nuevaVelocidad <= 255){
+      velocidad = nuevaVelocidad;
+      analogWrite(ENA, velocidad);
+      analogWrite(ENB, velocidad);
+      Serial.println("Nueva velocidad: " + String(velocidad));
+      server.send(200, "text/plain", "Nueva velocidad procesada" + velocidad);
+    }
+  } else {
+    server.send(400, "text/plain", "No se recibió velocidad");
+  }
+}
+void setTiempo(){
+  server.sendHeader("Access-Control-Allow-Origin", "*");
+  if (server.hasArg("plain")) {
+    int nuevoTiempo = server.arg("plain").toInt();
+    if(nuevoTiempo >=0 && nuevoTiempo <= 10){
+      tiempo = nuevoTiempo * 1000;
+      Serial.println("Nuevo tiempo: " + String(tiempo));
+      server.send(200, "text/plain", "Nueva tiempo procesado" + tiempo);
+    }
+  } else {
+    server.send(400, "text/plain", "No se recibió velocidad");
+  }
 }
 void loop() {
   server.handleClient();
